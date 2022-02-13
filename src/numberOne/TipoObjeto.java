585870a05f3a5f8bd39d8fe12a3cbd8e2cf26f9a
ReynosoRocio <89603734+ReynosoRocio@users.noreply.github.com>
2022-02-13 17:53:13 -0600

    agregar apuntes tipo objeto

---
+++ src/numberOne/TipoObjeto.java
@@ -0,0 +1,43 @@
+/**
+ * Create By Rocio RR
+ *13 feb. 2022
+ */
+package numberOne;
+
+/**
+ * @author Rocio
+ *
+ */
+public class TipoObjeto {
+	/****
+	 * Tipo											Descripción													Valor por defecto
+	 * 
+	 * Tipos de la biblioteca						String(cadenas de texto)
+	 * estándar de Java 							Otros ejemplos Scanner, TreeSet,ArrayList						NULL
+	 * 
+	 * Tipos definidos por el usuario/programador	Cualquiera que sea necesario									NULL
+	 * 
+	 * arrays										Serie de elemntos o formación tipo vector o matriz.
+	 * 												Lo consideramos un objeto especial que carece de métodos		NULL
+	 * 
+	 * Tipos envoltorio o wrapper					Byte
+	 * (Equivalentes a los tipos primitivos pero	Short
+	 * como objetos)								Integer
+	 * 												Long
+	 * 												Float
+	 * 												Double
+	 * 												Character
+	 * 
+	 */
+	//Declaración de wrappers antes de Java 9
+	//Byte fByte= new Byte(1);
+	
+	//Declaración de Wrappers después de Java 9
+	Byte fByte0= 2;
+	Short fShort0;
+	Integer fInteger0;
+	Long fLong0;
+	Float fFloat0;
+	Double fDouble0;	
+	Boolean fBoolean;
+}
